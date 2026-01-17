25fde87cec565e474b00cc8f01263f5b5acc7db0
IndianGameDevNco <bhasins2020@gmail.com>
2026-01-17 18:30:57 +0530

    Coded caesar_cipher

---
+++ caesar_cipher.rb
@@ -0,0 +1,10 @@
+def caesar_cipher string, key 
+  alphabet = ('a'..'z').to_a
+  string.chars.map do |char| 
+    next char unless alphabet.include?(char.downcase)
+    temp = alphabet[(alphabet.index(char.downcase) + key) % 26]
+    char == char.upcase ? temp.upcase : temp
+  end.join
+end
+
+p caeser_cipher gets, gets.to_i
