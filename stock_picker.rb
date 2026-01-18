a2d87027054c9e38435af099d183fb03f6304c06
IndianGameDevNco <bhasins2020@gmail.com>
2026-01-18 13:13:55 +0530

    Coded stock_picker.rb

---
+++ stock_picker.rb
@@ -0,0 +1,19 @@
+def stock_picker(days) 
+  n = days.length - 1
+  best_profit = '-inf'.to_i
+  buy_day = 0
+  sell_day = 0
+  days.each_with_index do |price, index|
+    days[index..n].each_with_index do |price2, index2|
+      profit = price2 - price
+      if profit > best_profit
+        best_profit = profit
+        buy_day = index
+        sell_day = index + index2
+      end
+    end
+  end
+  [buy_day, sell_day]
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10]) # => [1,4]
