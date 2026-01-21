9725f6c2aa842f7936d9adbe5ac8e753fd089af9
IndianGameDevNco <bhasins2020@gmail.com>
2026-01-21 16:34:16 +0530

    Coded bubble_sort.rb

---
+++ bubble_sort.rb
@@ -0,0 +1,11 @@
+def bubble_sort(arr) 
+  n = arr.length
+  arr.each_with_index do |i_value, i|
+    arr.each_with_index do |j_value, j|
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if j < n - i - 1 && j_value > arr[j + 1]
+    end
+  end
+end
+
+p bubble_sort([64, 34, 25, 12, 22, 11, 90])
+# Output: [11, 12, 22, 25, 34, 64, 90]
